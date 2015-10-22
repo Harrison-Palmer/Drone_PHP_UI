a4de9a5b1e535d24fde4716481f33a8885e6a714
Harrison <hpalmer@student.dwc.edu>
2015-10-22 15:44:48 -0400

    yes
    yes

---
+++ Drone_UI.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--Harrison Palmer
+CS203
+fall 2015-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo "Drone Management";?></title>
+</head>
+
+<body bgcolor="#C47806">
+</body>
+
+</html>
